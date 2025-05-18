e1e1e0b634c266d30241665cb6091ad777cb4fbc
Afia Maham <153532749+AfiaMaham@users.noreply.github.com>
2025-05-18 18:53:22 +0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (97.23%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,5 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        list1 = []
+        list2 = []
+        list3 = []
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                list1.append(0)
+            elif nums[i] == 1:
+                list2.append(1)
+            else:
+                list3.append(2)
+        nums.clear()
+        list4 = list1+list2+list3
+        for i in list4:
+            nums.append(i)
+        
         
