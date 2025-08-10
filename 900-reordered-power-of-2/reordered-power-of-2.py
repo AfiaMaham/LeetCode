224ad7b7d1f2a1f28ba6c9c8155c5ba6d24cecbe
Afia Maham <153532749+AfiaMaham@users.noreply.github.com>
2025-08-10 18:00:00 +0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (53.05%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.py
+++ 900-reordered-power-of-2/reordered-power-of-2.py
@@ -1,28 +1,8 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        powers=[]
-        for i in range(30):
-            powers.append(2**i)
-        n=str(n)
-        l=len(n)
-        possible=[]
-        real_count=defaultdict(int)
-        check_count=defaultdict(int)
-        for i in range(l):
-            real_count[n[i]]+=1
-        for power in powers:
-            if len(str(power))==l:
-                possible.append(power)
-        for power in possible:
-            check_count=defaultdict(int)
-            power=str(power)
-            for i in range(len(power)):
-                check_count[power[i]]+=1
-            if check_count==real_count:
+        target = ''.join(sorted(str(n)))
+        
+        for i in range(30):  
+            if ''.join(sorted(str(1 << i))) == target:
                 return True
         return False
-            
-
-        
-
-        
